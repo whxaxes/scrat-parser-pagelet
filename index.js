e5e07383cb42bb05aa3ec0ccd4a4968427ab15e6
wanghx <whx89768@alibaba-inc.com>
2017-08-04 11:20:38 +0800

    feat: support absolute alias

--- index.js
+++ index.js
@@ -60,14 +60,16 @@ function normalizeUrl(url, options) {
   if (url.charAt(0) === '.' && !path.extname(url)) {
     url = path.resolve(path.dirname(file.realpath), url)
       .replace(options.baseDir, '');
+  } else if (path.isAbsolute(url)) {
+    url = path.relative(options.baseDir, url);
+  }
 
-    if (path.sep !== '/') {
-      url = url.replace(/\\/g, '/');
-    }
+  if (path.sep !== '/') {
+    url = url.replace(/\\/g, '/');
+  }
 
-    if (url.charAt(0) === '/') {
-      url = url.substring(1);
-    }
+  if (url.charAt(0) === '/') {
+    url = url.substring(1);
   }
 
   return quot + url + quot;
